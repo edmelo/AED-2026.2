bfd9e8c5fab39073f94b90f3e5f0c1de05f1d485
edmelo <ednaldo_melo@hotmail.com>
2026-08-28 00:17:01 +0000

    metodo selectionSort Python

---
+++ ordenacao.py
@@ -0,0 +1,18 @@
+def selection_sort(valores):
+	"""Ordena uma lista em ordem crescente usando seleção direta."""
+	lista = valores.copy()
+
+	for i in range(len(lista) - 1):
+		menor = i
+		for j in range(i + 1, len(lista)):
+			if lista[j] < lista[menor]:
+				menor = j
+
+		if menor != i:
+			lista[i], lista[menor] = lista[menor], lista[i]
+
+	return lista
+
+"""testa o metodo com alguns valores."""
+a = [5, 4, 3, 2, 1, 3]
+print(selection_sort(a))
