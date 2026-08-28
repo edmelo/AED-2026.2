b707e24050a32a3300aed7a1f732e129c2c3c8b9
edmelo <ednaldo_melo@hotmail.com>
2026-08-28 00:23:57 +0000

    implementacao insertion sort

--- ordenacao.py
+++ ordenacao.py
@@ -16,3 +16,23 @@ def selection_sort(valores):
 """testa o metodo com alguns valores."""
 a = [5, 4, 3, 2, 1, 3]
 print(selection_sort(a))
+
+def insertion_sort(valores):
+    """Ordena uma lista em ordem crescente usando inserção direta."""
+    lista = valores.copy()
+
+    for i in range(1, len(lista)):
+        chave = lista[i]
+        j = i - 1
+
+        while j >= 0 and lista[j] > chave:
+            lista[j + 1] = lista[j]
+            j -= 1
+
+        lista[j + 1] = chave
+
+    return lista
+
+"""testa o metodo com alguns valores."""
+b = [5, 4, 3, 2, 1, 3]
+print(insertion_sort(b))
